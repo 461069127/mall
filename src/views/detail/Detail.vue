e1ac48209f51dcc7979f1fb1cddea25d93ea478f
yzy5632008 <461069127@qqcom>
2021-06-22 17:37:56 +0800

    1.0.0

---
+++ src/views/detail/Detail.vue
@@ -0,0 +1,160 @@
+<template>
+  <div id="detail">
+    <detail-nav-bar class="detail-nav" @itemClick="itemClick"/>
+    <scroll class="content" 
+            ref="scroll" 
+            :probe-type="3"
+            @scroll="contentScroll" >
+      <detail-swiper :top-images="topImages"/>
+      <detail-base-info :goods="goods"/>
+      <detail-shop-info :shop="shop"/>
+      <detail-goods-info :detail-info="detailInfo" @imageLoad="imageLoad"/>
+      <detail-param-info :param-info="paramInfo" ref="paramsRef"/>
+      <detail-comment :comment="comment" ref="commentRef"/>
+      <goods-list :goods="recommends" ref="recommendRef"/>
+    </scroll>
+    <detail-bottom-bar/>
+    <back-top v-show="isShowTop" @click.native="backTop"/>
+  </div>
+</template>
+
+<script>
+  import DetailNavBar from './childComps/DetailNavBar'
+  import DetailSwiper from './childComps/DetailSwiper'
+  import DetailBaseInfo from './childComps/DetailBaseInfo'
+  import DetailShopInfo from './childComps/DetailShopInfo'
+  import DetailGoodsInfo from './childComps/DetailGoodsInfo'
+  import DetailParamInfo from './childComps/DetailParamInfo'
+  import DetailComment from './childComps/DetailComment.vue'
+  import DetailBottomBar from './childComps/DetailBottomBar.vue'
+
+  import Scroll from 'components/common/scroll/Scroll'
+  import GoodsList from 'components/content/goods/GoodsList.vue'
+  import BackTop from 'components/content/backTop/BackTop.vue'
+
+  import {getDetail, Goods, Shop, GoodsParam, getRecommend } from "network/detail";
+  import { debounce } from 'common/utils'
+
+  export default {
+    name: "Detail",
+    components: {
+      DetailNavBar,
+      DetailSwiper,
+      DetailBaseInfo,
+      DetailShopInfo,
+      DetailGoodsInfo,
+      DetailParamInfo,
+      DetailComment,
+      DetailBottomBar,
+
+      Scroll,
+      GoodsList,
+      BackTop,
+    },
+    data() {
+      return {
+        iid: null,
+        topImages: [],
+        goods: {},
+        shop: {},
+        detailInfo: {},
+        paramInfo: {},
+        comment: {},
+        recommends: [],
+        themePosY: [],
+        itemImgListener: null,
+        getThemePosY: null,
+        newRefresh: null,
+        isShowTop: false
+      }
+    },
+    created() {
+      // 1.保存传入的iid
+      this.iid = this.$route.params.iid
+
+      // 2.根据iid请求详情数据
+      getDetail(this.iid).then(res => {
+        // 1.获取顶部的图片轮播数据
+        const data = res.result;
+        this.topImages = data.itemInfo.topImages
+
+        // 2.获取商品信息
+        this.goods = new Goods(data.itemInfo, data.columns, data.shopInfo.services)
+
+        // 3.创建店铺信息的对象
+        this.shop = new Shop(data.shopInfo)
+
+        // 4.保存商品的详情数据
+        this.detailInfo = data.detailInfo;
+
+        // 5.获取参数的信息
+        this.paramInfo = new GoodsParam(data.itemParams.info, data.itemParams.rule)
+        
+        // 6.获取评论的信息
+        this.comment =data.rate.list[0]
+      })
+
+        // 7.获取推荐的信息
+      getRecommend().then(res =>{
+        this.recommends = res.data.list
+      })
+
+      this.getThemePosY = debounce(() => {
+        this.themePosY = [0];
+        this.themePosY.push(-this.$refs.paramsRef.$el.offsetTop + 44);
+        this.themePosY.push(-this.$refs.commentRef.$el.offsetTop + 44);
+        this.themePosY.push(-this.$refs.recommendRef.$el.offsetTop + 44);
+        console.log(this.themePosY);
+      }, 100);   
+    },
+    mounted(){
+      this.newRefresh = debounce(() =>{this.$refs.scroll.refresh()})
+      this.itemImgListener = () =>{
+        this.newRefresh()
+        this.getThemePosY()
+      }
+      this.$bus.$on('itemImageLoad', this.itemImgListener)
+    },
+    beforeDestroy() {
+      this.$bus.$off('itemImageLoad', this.itemImgListener)
+    },
+
+    methods: {     
+      imageLoad() {
+        this.$refs.scroll.refresh()
+      },
+      
+      itemClick(index) {
+        this.$refs.scroll.scrollTo(0, this.themePosY[index], 700);
+      },
+
+      contentScroll(position){
+        this.isShowTop = -position.y > 1000
+        console.log(position.y);
+      },
+
+      backTop(){
+        this.$refs.scroll.scrollTo(0, 0, 700)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  #detail {
+    position: relative;
+    z-index: 9;
+    background-color: #fff;
+    height: 100vh;
+  }
+
+  .detail-nav {
+    position: relative;
+    z-index: 9;
+    background-color: #fff;
+  }
+
+  .content {
+    height: calc(100% - 44px - 2.09rem);
+  }
+</style>
---
+++ src/views/detail/childComps/DetailComment.vue
@@ -0,0 +1,95 @@
+<template>
+  <div id="detail_comment" v-if="Object.keys(comment).length != 0">
+    <div class="comment_title">
+      <span class="comment_name">用户评价</span>
+      <span class="comment_more">更多</span>
+    </div>
+    <div class="comment_info">
+      <div class="u_info">
+        <img :src="comment.user.avatar" alt="" />
+        <span>{{ comment.user.uname }}</span>
+      </div>
+      <div class="comment_content">
+        <div class="content">{{ comment.content }}</div>
+        <p class="content_info">
+          {{ comment.created | dateShow }} {{ comment.style }}
+        </p>
+      </div>
+    </div>
+    <div class="comment_img" v-if="comment.images != null">
+      <img
+        :src="item"
+        alt=""
+        v-for="(item, index) in comment.images"
+        :key="index"
+      />
+    </div>
+  </div>
+</template>
+<script>
+import { formatDate } from "common/utils.js";
+export default {
+  props: {
+    comment: {
+      type: Object,
+      default() {
+        return {};
+      }
+    }
+  },
+  filters: {
+    dateShow(value) {
+      let date = new Date(value * 1000);
+      return formatDate(date, "yyyy-MM-dd");
+    }
+  }
+};
+</script>
+<style scoped>
+#detail_comment {
+  font-size: 0.65rem;
+  padding: 0.42rem 0.34rem;
+  border-bottom: 5px solid #f2f5f8;
+}
+.comment_title {
+  padding: 0.42rem 0;
+  padding-bottom: 0.8rem;
+  border-bottom: 0.08rem solid rgba(128, 128, 128, 0.3);
+  position: relative;
+}
+
+.comment_more {
+  position: absolute;
+  right: -0.26rem;
+}
+
+.u_info {
+  padding: 0.42rem 0;
+  line-height: 1.8rem;
+  display: flex;
+}
+.u_info img {
+  width: 1.8rem;
+  height: 1.8rem;
+  border-radius: 50%;
+  border: 0.04rem solid rgba(128, 128, 128, 0.3);
+}
+.u_info span {
+  margin-left: 0.42rem;
+}
+
+.content {
+  line-height: 1rem;
+  color: #666;
+}
+.content_info {
+  color: #999;
+  padding: 0.3rem 0;
+}
+
+.comment_img img {
+  margin-right: 0.21rem;
+  width: auto;
+  height: 4.26rem;
+}
+</style>
